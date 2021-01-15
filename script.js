104b97538d5ab30ac1af69bfb9e3dd7a2b57da89
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2021-01-15 16:24:55 +0000

    add swiper js function but needs a fix

--- script.js
+++ script.js
@@ -44,3 +44,22 @@ video.addEventListener('timeupdate', () => {
     
 })
 // End video player
+
+// Start Swiper js
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+// End Swiper js
