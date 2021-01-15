198c32bfa3d1dfd906d92c9334500b21478ba130
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2021-01-15 13:38:19 +0000

    make video play/pause btn work in script.js file

--- script.js
+++ script.js
@@ -9,4 +9,26 @@ menu.addEventListener('click', () => {
 // End Navbar
 
 // Start video player
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar') 
+
+const playPause = () => {
+
+    if(video.paused) {
+        video.play()
+        btn.className = 'fas fa-pause-circle'
+        video.style.opacity = '0.7'
+    } else {
+
+        video.pause()
+        btn.className = 'fas fa-play-circle'
+        video.style.opacity = '0.3'
+    }
+    
+}
+
+btn.addEventListener('click', () => {
+    playPause()
+})
 // End video player
